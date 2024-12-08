1d613d41ac2d47bc5cfc555b5ef64df9be37f3ae
KyleFlyn <144923405+KyleFlyn@users.noreply.github.com>
2024-12-08 23:48:13 +0000

    Working Code with filters
    Filters added and styling added

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,25 @@ const apiHost = 'zillow-com4.p.rapidapi.com';
     
 document.getElementById('getHouseButton').addEventListener('click', async () => {
     const selectedCities = [];
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            selectedCities.push(checkboxes[i].value);
+    const selectedType = [];
+    const cityCheckboxes = document.querySelectorAll('#city input[type="checkbox"]');
+    const typeCheckboxes = document.querySelectorAll('#type input[type="checkbox"]');
+    for (let i = 0; i < cityCheckboxes.length; i++) {
+        if (cityCheckboxes[i].checked) {
+            selectedCities.push(cityCheckboxes[i].value);
+        }
+    }
+
+    
+    for (let i = 0; i < typeCheckboxes.length; i++) {
+        if (typeCheckboxes[i].checked) {
+            selectedType.push(typeCheckboxes[i].value);
         }
     }
     
-        
-    if (selectedCities.length === 0) {
-        alert('Please select at least one city.');
+    const topDiv = document.getElementById('house');
+    if (selectedCities.length === 0 || selectedType.length === 0) {
+        topDiv.innerHTML = '<p>Please select at least one city and type.</p>';
         return;
     }
 
@@ -21,38 +30,34 @@ document.getElementById('getHouseButton').addEventListener('click', async () => 
    houseDiv.innerHTML = '<p>Loading house data...</p>';
 
     const filteredResults = [];
-    const filteredData = [];
     for (let i = 0; i < selectedCities.length; i++) {
-        const result = await fetchCity(selectedCities[i]);
-        console.log(`Humidity for ${result?.name}: ${result?.main?.humidity}`); // Debugging line        
+        const result = await fetchCity(selectedCities[i],selectedType[i]);
         filteredResults.push(result);
     }
 
-    if (filteredResults.length === 0) {
-       houseDiv.innerHTML = `<p>No cities meet the minimum price requirement of ${minPrice}%.</p>`;
-        return;
-    }
-    
     let weatherHTML = '';
     const houses = filteredResults[0].data; // Access the array of houses
 
     for (let i = 0; i < houses.length; i++) {
         const house = houses[i];
+        const imageUrl = house.media.propertyPhotoLinks.highResolutionLink || 'https://via.placeholder.com/300x450?text=No+Image';
         weatherHTML += `
             <div class="container">
-                <p><strong>City:</strong> ${house.address.streetAddress}</p>
-                <p><strong>Country:</strong> ${house.country}</p>
-                <p><strong>Price: $</strong> ${house.price?.value || "N/A"}</p>
-                <p><strong>Bedrooms:</strong> ${house.bedrooms}</p>
-                <p><strong>Bathrooms:</strong> ${house.bathrooms}</p>
+                   <p><strong>Status:</strong> ${house.listing?.listingStatus || house.listingStatus || 'Unknown'} </p>
+                    <img src="${imageUrl}" alt="${house.price?.value}" style="max-width: 100%; height: auto; border-radius: 10px;" />
+                    <p><strong>Address:</strong> ${house.address.streetAddress}</p>
+                    <p><strong>Country:</strong> ${house.country}</p>
+                    <p><strong>Price: $</strong> ${house.price?.value || "N/A"}</p>
+                    <p><strong>Bedrooms:</strong> ${house.bedrooms}</p>
+                    <p><strong>Bathrooms:</strong> ${house.bathrooms}</p>
             </div>
         `;
     }
     houseDiv.innerHTML = weatherHTML;
 });
     
-async function fetchCity(city) {
-    const url = `https://zillow-com4.p.rapidapi.com/properties/search?location=${city}&listingType=agent`;
+async function fetchCity(city, type) {
+    const url = `https://zillow-com4.p.rapidapi.com/properties/search?location=${city}&status=${type}&sort=relevance&priceType=listPrice&listingType=agent`;
     const options = {
         method: 'GET',
         headers: {
@@ -64,7 +69,7 @@ async function fetchCity(city) {
     try {
         const response = await fetch(url, options);
         if (!response.ok) {
-            throw new Error(`Error fetching weather for ${city}: ${response.status}`);
+            throw new Error(`Error fetching houses for ${city}: ${response.status}`);
         }
         return await response.json();
     } catch (error) {
