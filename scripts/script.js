e45370448ca3ddb5801bf84ac90f8cd9d41ee5a0
KyleFlynn1 <s00251601@atu.ie>
2024-12-09 17:48:34 +0000

    Fixed second endpoint to work
    Second Endpoint for house previous price sold at working and sort by higher and lower prices added

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
-const apiKey = '67ebc96918mshf722694c1c0425cp18a43cjsn4654236c4225';
+const apiKey = '61d3ea00eemsh04cd69a0ce65e77p1b227cjsnd38242e62bd2';
 const apiHost = 'zillow-com4.p.rapidapi.com';
     
 document.getElementById('getHouseButton').addEventListener('click', async () => {
     const selectedCities = [];
     const selectedType = [];
+    const selectedSortType = [];
     const cityCheckboxes = document.querySelectorAll('#city input[type="checkbox"]');
     const typeCheckboxes = document.querySelectorAll('#type input[type="checkbox"]');
+    const sortTypeCheckboxes = document.querySelectorAll('#sortType input[type="checkbox"]');
     for (let i = 0; i < cityCheckboxes.length; i++) {
         if (cityCheckboxes[i].checked) {
             selectedCities.push(cityCheckboxes[i].value);
@@ -18,32 +20,42 @@ document.getElementById('getHouseButton').addEventListener('click', async () => 
             selectedType.push(typeCheckboxes[i].value);
         }
     }
+
+    for (let i = 0; i < sortTypeCheckboxes.length; i++) {
+        if (sortTypeCheckboxes[i].checked) {
+            selectedSortType.push(sortTypeCheckboxes[i].value);
+        }
+    }
     
     const topDiv = document.getElementById('house');
-    if (selectedCities.length === 0 || selectedType.length === 0) {
+    if (selectedCities.length === 0 || selectedType.length === 0 || sortType.length === 0) {
         topDiv.innerHTML = '<p>Please select at least one city and type.</p>';
         return;
     }
+
+    
 
     
     const houseDiv = document.getElementById('house');
    houseDiv.innerHTML = '<p>Loading house data...</p>';
 
     const filteredResults = [];
+    const estimatedResults = [];
     for (let i = 0; i < selectedCities.length; i++) {
-        const result = await fetchCity(selectedCities[i],selectedType[i]);
+        const result = await fetchCity(selectedCities[i],selectedType[i], selectedSortType[i]);
         filteredResults.push(result);
     }
 
-    let weatherHTML = '';
+    let houseHTMl = '';
     const houses = filteredResults[0].data; // Access the array of houses
 
     for (let i = 0; i < houses.length; i++) {
         const house = houses[i];
         const imageUrl = house.media.propertyPhotoLinks.highResolutionLink || 'https://via.placeholder.com/300x450?text=No+Image';
+        const estimateResult = await fetchHouseEstimate(house.zpid);
         if (selectedType[0] === "forRent")
         {
-            weatherHTML += `
+            houseHTMl += `
                 <div class="container">
                     <p><strong>Status:</strong> ${house.listing?.listingStatus || house.listingStatus || 'Unknown'} </p>
                         <img src="${imageUrl}" alt="${house.price?.value}" style="max-width: 100%; height: auto; border-radius: 10px;" />
@@ -52,11 +64,13 @@ document.getElementById('getHouseButton').addEventListener('click', async () => 
                         <p><strong>Rental Type: </strong> ${house.groupType || "N/A"}</p>
                         <p><strong>Min Price:</strong> $ ${house.minPrice}</p>
                         <p><strong>Max Price:</strong> $${house.maxPrice}</p>
+                        <p><strong>First Sale Date:</strong> ${estimateResult.data.priceHistory[0].date}</p>
+                        <p><strong>First Sale Price:</strong> $${estimateResult.data.priceHistory[0].price}</p>
                 </div>
             `;
         }
         else{ 
-            weatherHTML += `
+            houseHTMl += `
                 <div class="container">
                     <p><strong>Status:</strong> ${house.listing?.listingStatus || house.listingStatus || 'Unknown'} </p>
                         <img src="${imageUrl}" alt="${house.price?.value}" style="max-width: 100%; height: auto; border-radius: 10px;" />
@@ -64,16 +78,17 @@ document.getElementById('getHouseButton').addEventListener('click', async () => 
                         <p><strong>Country:</strong> ${house.country}</p>
                         <p><strong>Price: $</strong> ${house.price?.value || "N/A"}</p>
                         <p><strong>Bedrooms:</strong> ${house.bedrooms}</p>
-                        <p><strong>Bathrooms:</strong> ${house.bathrooms}</p>
+                        <p><strong>First Sale Date:</strong> ${estimateResult.data.priceHistory[0].date}</p>
+                        <p><strong>First Sale Price:</strong> $${estimateResult.data.priceHistory[0].price}</p>
                 </div>
             `;
         }
     }
-    houseDiv.innerHTML = weatherHTML;
+    houseDiv.innerHTML = houseHTMl;
 });
     
-async function fetchCity(city, type) {
-    const url = `https://zillow-com4.p.rapidapi.com/properties/search?location=${city}&status=${type}&sort=relevance&priceType=listPrice&listingType=agent`;
+async function fetchCity(city, type, sortType) {
+    const url = `https://zillow-com4.p.rapidapi.com/properties/search?location=${city}&status=${type}&sort=price&sortType=${sortType}&priceType=listPrice&listingType=agent`;
     const options = {
         method: 'GET',
         headers: {
@@ -93,3 +108,27 @@ async function fetchCity(city, type) {
         return null;
     }
 }
+
+    
+async function fetchHouseEstimate(zpid) {
+    const url = `https://zillow-com4.p.rapidapi.com/properties/price-tax-history?zpid=${zpid}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey,
+            'x-rapidapi-host': apiHost
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`Error fetching houses for ${zpid}: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
